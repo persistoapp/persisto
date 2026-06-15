4d70e52312a694a2137ad4dacc9321e274eca0fa
persistoapp <persisto1pc@gmail.com>
2026-06-15 20:03:08 +0200

    Add files via upload

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -15,7 +15,7 @@ exports.handler = async function(event) {
       },
       body: JSON.stringify({
         model: "claude-haiku-4-5-20251001",
-        max_tokens: 1000,
+        max_tokens: 2000,
         system: body.system,
         messages: body.messages
       })
